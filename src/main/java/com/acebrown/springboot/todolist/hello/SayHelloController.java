53b4f1ba01bf4c3e432921c5855a5fd45a917899
ace-brown <kazem_abdi@yahoo.com>
2023-09-08 16:17:07 +0200

    inital commit

---
+++ src/main/java/com/acebrown/springboot/todolist/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.acebrown.springboot.todolist.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello World";
+	}
+}
